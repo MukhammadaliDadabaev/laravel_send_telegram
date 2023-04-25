0510eb3b48d0ebb5c667a8058d78c3c73910bf6d
Mukhammadali <ali_6017@mail.ru>
2023-04-25 20:27:09 +0500

    product message telegram

--- routes/web.php
+++ routes/web.php
@@ -1,76 +1,22 @@
 <?php
 
-use App\Helpers\Telegram;
+use App\Http\Controllers\OrderController;
+use App\Http\Controllers\WebhookController;
+use App\Models\Order;
+use Illuminate\Support\Facades\Http;
 use Illuminate\Support\Facades\Route;
 
-/*
-|--------------------------------------------------------------------------
-| Web Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register web routes for your application. These
-| routes are loaded by the RouteServiceProvider within a group which
-| contains the "web" middleware group. Now create something great!
-|
-*/
-//-------------> sendMessage, sendDocument
-// Route::get('/', function (Telegram $telegram) {
-//     $sendMessage = $telegram->sendMessage(261944697, 'Message');
-//     $sendMessage = json_decode($sendMessage);
-//     $telegram->sendDocument(261944697, 'shash.jpg', $sendMessage->result->message_id);
-// });
+// https://aliportfolio.uz/,
+// https://api.telegram.org/bot5523403818:AAEoWPnWkxHn9FX0kbZAuL7AscjFR4z701c/setWebhook?url=https://aliportfolio.uz/webhook
 
-
-// Route::get('/', function (Telegram $telegram) {
-//     $buttons = [
-//         'inline_keyboard' => [
-//             [
-//                 [
-//                     'text' => 'button-1',
-//                     'callback_data' => '1',
-//                 ],
-//                 [
-//                     'text' => 'button-2',
-//                     'callback_data' => '2',
-//                 ],
-//             ],
-//             [
-//                 [
-//                     'text' => 'button-3',
-//                     'callback_data' => '3',
-//                 ]
-//             ]
-//         ]
-
-//     ];
-//     $sendMessage = $telegram->sendButtons(261944697, 'Knopka', json_encode($buttons));
-//     $sendMessage = json_decode($sendMessage);
-//     dd($sendMessage);
-// });
-
-Route::get('/', function (Telegram $telegram) {
-    $buttons = [
-        'inline_keyboard' => [
-            [
-                [
-                    'text' => 'button-4',
-                    'callback_data' => '4',
-                ],
-                [
-                    'text' => 'button-5',
-                    'callback_data' => '5',
-                ],
-            ],
-            [
-                [
-                    'text' => 'button-6',
-                    'callback_data' => '6',
-                ]
-            ]
-        ]
-
-    ];
-    $sendMessage = $telegram->sendButtons(261944697, 'Knopka-edit', json_encode($buttons), 173);
-    $sendMessage = json_decode($sendMessage);
-    dd($sendMessage);
+Route::get('/', function (Order $order) {
+    // $http = Http::get('https://api.telegram.org/bot5523403818:AAEoWPnWkxHn9FX0kbZAuL7AscjFR4z701c/getWebhookInfo?');
+    // dd(json_decode($http->body()));
+    return view('site.order', ['orders' => $order->active()->get()]);
 });
+
+// Route::group(['namespace' => 'App\Http\Controllers'], function () {
+Route::post('/order/store', [OrderController::class, 'store'])->name('order.store');
+Route::get('/webhook', [WebhookController::class, 'index']);
+    // Route::post('/post/store', 'PostController@store')->name('post.store');
+// });
